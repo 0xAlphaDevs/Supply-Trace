67c3197e8c4cef259662d40c4dbacfa8abd977c0
0xyshv <writetoyshv@gmail.com>
2024-04-10 18:33:09 +0530

    chore: configure wagmi & connectkit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button";
+"use client"
+
+import { ConnectKitButton } from "connectkit";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 font-bold text-3xl">
       Welcome to Supply Trace
-      <Button>Click me</Button>
+      <ConnectKitButton />
     </main>
   );
 }
