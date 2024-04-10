a74d2bd38f66629ab40e82b9d5bcaa883e42d5c3
0xyshv <writetoyshv@gmail.com>
2024-04-10 18:07:12 +0530

    chore: Add required dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 font-bold text-3xl">
       Welcome to Supply Trace
+      <Button>Click me</Button>
     </main>
   );
 }
