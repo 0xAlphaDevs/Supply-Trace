22f0e2b2f01f728a71c14e48c0426e20bf15ae2e
0xyshv <writetoyshv@gmail.com>
2024-04-17 10:48:39 +0530

    feat: add skeleton for view history

--- components/inventory/viewHistory.tsx
+++ components/inventory/viewHistory.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton"
 
 const ViewHistory = ({ attestationId }: { attestationId: string }) => {
     return (
-        <div>ViewHistory</div>
+        <div className='flex flex-col items-center gap-8'>
+            <p className='flex justify-center text-4xl font-semibold'>View History</p>
+            {/* //Skeleton */}
+            <div className='flex flex-col gap-16 pt-8'>
+                <div className="flex items-center space-x-4">
+                    <Skeleton className="h-24 w-24 rounded-full bg-slate-200" />
+                    <div className="space-y-2">
+                        <Skeleton className="h-10 w-[250px] bg-slate-200" />
+                        <Skeleton className="h-10 w-[200px] bg-slate-200" />
+                    </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                    <Skeleton className="h-24 w-24 rounded-full bg-slate-200" />
+                    <div className="space-y-2">
+                        <Skeleton className="h-10 w-[250px] bg-slate-200" />
+                        <Skeleton className="h-10 w-[200px] bg-slate-200" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
